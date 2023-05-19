60c6058a3814bbe11e8b66fbcd7e5e7657bf627a
Rakesh Bhat <bhatrakesh@gmail.com>
2023-05-19 11:24:42 +0530

    new commit

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "security-group" {
+ default= ""
+}
+
+variable "subnet" {
+ default= ""
+}
+
+variable "ami" {
+  default = ""
+}
+
+variable "type" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
